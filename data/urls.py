3fc89fcee9a8ffc705e1ede2e679ae75d11020de
Nurmukhammed <86832241+narymbaev@users.noreply.github.com>
2022-11-17 19:43:09 +0600

    added templates AND Urls AND Views

--- data/urls.py
+++ data/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import data
+from .views import diseaseType, country, disease, discover, users, publicServant, doctor, specialize, record
 
 urlpatterns = [
-    path('', data, name='data'),
+    path('disease-type/', diseaseType, name='disease-type'),
+    path('country/', country, name='country'),
+    path('disease/', disease, name='disease'),
+    path('discover/', discover, name='discover'),
+    path('users/', users, name='users'),
+    path('public-servant/', publicServant, name='public-servant'),
+    path('doctor/', doctor, name='doctor'),
+    path('specialize/', specialize, name='specialize'),
+    path('record/', record, name='record'),
 ]
--- data/views.py
+++ data/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
-from .models import Country
+from .models import DiseaseType, Country, Disease, Discover, Users, PublicServant, Doctor, Specialize, Record 
 
 # Create your views here.
-def data(request):
-    data = Country.objects.all()
-    context = {'data': data}
-    return render(request, 'data.html', context)
+def diseaseType(request):
+    disease_type = DiseaseType.objects.all()
+    context = {'disease_type': disease_type}
+    return render(request, 'disease_type.html', context)
+
+def country(request):
+    country = Country.objects.all()
+    context = {'country': country}
+    return render(request, 'country.html', context)
+
+def disease(request):
+    disease = Disease.objects.all()
+    context = {'disease': disease}
+    return render(request, 'disease.html', context)
+
+def discover(request):
+    discover = Discover.objects.all()
+    context = {'discover': discover}
+    return render(request, 'discover.html', context)
+
+def users(request):
+    users = Users.objects.all()
+    context = {'users': users}
+    return render(request, 'user.html', context)
+
+def publicServant(request):
+    public_servants = PublicServant.objects.all()
+    context = {'public_servants': public_servants}
+    return render(request, 'public_servant.html', context)
+
+def doctor(request):
+    doctors = Doctor.objects.all()
+    context = {'doctors': doctors}
+    return render(request, 'doctor.html', context)
+
+def specialize(request):
+    specializes = Specialize.objects.all()
+    context = {'specializes': specializes}
+    return render(request, 'specialize.html', context)
+
+def record(request):
+    records = Record.objects.all()
+    context = {'records': records}
+    return render(request, 'record.html', context)
